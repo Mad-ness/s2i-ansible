5eab069cdcfd4ca375ae0df48cd1a3e940e78ee4
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2019-05-03 21:27:15 -0400

    Updatedd run.sh

--- examples/lab1/run.sh
+++ examples/lab1/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /privkeys/id_rsa > /tmp/id_rsa && chmod 0400
+cat /privkeys/id_rsa > /tmp/id_rsa && chmod 0400 /tmp/id_rsa
 
 ansible-playbook --private-key /tmp/id_rsa  site.yaml -vvv
 
